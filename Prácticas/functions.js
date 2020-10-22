4720b3c6e3216d1fddd89b4c0af1670c08051f5a
Elena <73283414+DamiselaElena@users.noreply.github.com>
2020-10-22 12:09:31 +0200

    Archivos iniciales

---
+++ Prácticas/functions.js
@@ -0,0 +1,58 @@
+// Asignatura
+document.getElementById("asignatura").innerHTML = "Planificación y administración de redes"
+
+// Table of Contents
+var c = function() {
+    return({
+        log: function(msg) {
+          consoleDiv = document.getElementById('console');
+          para = document.createElement('p');
+          text = document.createTextNode(msg);
+          para.appendChild(text);
+          consoleDiv.appendChild(para);
+        }
+    });
+}();
+
+window.onload = function () {
+    var toc = "";
+    var level = 1;
+    var maxLevel = 3;
+
+    document.getElementById("contents").innerHTML =
+        document.getElementById("contents").innerHTML.replace(
+            /<h([\d])>(.+)<\/h([\d])>/gi,
+            function (str, openLevel, titleText, closeLevel) {
+                if (openLevel != closeLevel) {
+				 c.log(openLevel)
+                    return str + ' - ' + openLevel;
+                }
+
+                if (openLevel > level) {
+                    toc += (new Array(openLevel - level + 1)).join("<ol>");
+                } else if (openLevel < level) {
+                    toc += (new Array(level - openLevel + 1)).join("</ol>");
+                }
+
+                level = parseInt(openLevel);
+
+                var anchor = titleText.replace(/ /g, "_");
+		var notags = anchor.replace(/<[^>]*>/g,'');
+				
+                toc += "<li><a href=\"#" + notags + "\">" + titleText
+                    + "</a></li>";
+
+                return "<h" + openLevel + "><a name=\"" + anchor + "\">"
+                    + titleText + "</a></h" + closeLevel + ">";
+            }
+        );
+
+    if (level) {
+        toc += (new Array(level + 1)).join("</ol>");
+    }
+
+    document.getElementById("toc").innerHTML += toc;
+};
+
+// Last modified date
+document.getElementById("modify").innerHTML = "Última modificación: " + document.lastModified
